af212973225d9a247730e752efaed92ae644ab67
David Mošna <davidmosna@Davids-MacBook-Air.local>
2023-08-07 19:52:00 +0200

    chore: add basic project scripts to package.json

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { Math, subtract } from './utils/math'
+
+console.log(subtract(1, 2))
+console.log(Math.add(1, 2))
---
+++ src/utils/math.ts
@@ -0,0 +1,9 @@
+export class Math {
+    static add(a: number, b: number): number {
+        return a + b
+    }
+}
+
+export const subtract = (a: number, b: number): number => {
+    return a - b
+}
